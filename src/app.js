77fa7b9bd7d40e1ef7d651439b9e2a36decca2b3
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-04-26 19:32:11 +0000

    avanzado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let num = Math.floor(Math.random() * 13) + 1;
+  if (num == 11) {
+    document.querySelector(".number").innerHTML = "J";
+  } else if (num == 12) {
+    document.querySelector(".number").innerHTML = "Q";
+  } else if (num == 13) {
+    document.querySelector(".number").innerHTML = "K";
+  } else if (num == 1) {
+    document.querySelector(".number").innerHTML = "A";
+  } else {
+    document.querySelector(".number").innerHTML = num;
+  }
+
+  let palo = ["♦", "♥", "♠", "♣"];
+  let numPalo = Math.floor(Math.random() * 3);
+  document.querySelector(".paloTop").innerHTML = palo[numPalo];
+  if (palo[numPalo] == "♦") {
+    let rombo = document.querySelector(".paloTop");
+    rombo.style.color = "red"
+  } else if (palo[numPalo] == "♥") {
+    let corazon = document.querySelector("paloTop");
+    corazon.style.color = "red"
+  } else if (palo[numPalo] == "♠"){
+    let picas = document.querySelector("paloTop");
+    picas.style.color = 
+  } else {
+    let trebol
+  }
 };
+
+
+// rotated.style.transform = 'rotate(90deg)
